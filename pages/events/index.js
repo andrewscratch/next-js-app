d30766b25a847fcf6bac08677482f8b13bf5022c
andrewscratch <andrew.chudlya@gmail.com>
2023-09-12 18:47:01 +0300

    add filter

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,23 @@
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
+import { Fragment } from "react";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
 
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
   return (
-    <div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </Fragment>
   );
 }
-
 export default AllEventsPage;
